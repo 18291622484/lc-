6e302208be149239738dc156fefb5d4f8a529934
18291622484 <18291622484@163.com>
2020-12-07 19:29:31 +0800

    剑指Offer56-I.数组中数字出现的次数 一个整型数组nums里除两个数字之外，其他数字都出现了两次。请写程序找出这两个只出现一次的数字。要求时间复杂度是O(n)，空间复杂度是O(1)。

---
+++ SwordRefersToOffer56-I.NumberTimesInArray/SwordRefersToOffer56-I.NumberTimesInArray.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int* singleNumbers(int* nums, int numsSize, int* returnSize) {
+	int* singlearr = (int*)calloc(2, sizeof(int)); if (singlearr == NULL)return NULL;
+	int sum = 0, count = 1;
+	for (int i = 0; i < numsSize; i++) {
+		sum ^= nums[i];
+	}
+	while ((sum & 1) == 0) {
+		sum >>= 1;
+		count *= 2;
+	}
+	for (int i = 0; i < numsSize; i++) {
+		if ((nums[i] & count) == 0) {
+			singlearr[0] ^= nums[i];
+		}
+		else {
+			singlearr[1] ^= nums[i];
+		}
+	}
+	return singlearr;
+}
+
+int main() {
+	int arr[] = { 1,2,10,4,1,5,7,5,8,7,8,4,3,3 };
+	int ret = 0;
+	int* retarr = singleNumbers(arr, sizeof(arr) / sizeof(arr[0]), &ret);
+	for (int i = 0; i < 2; i++) {
+		printf("%d\n", retarr[i]);
+	}
+	return 0;
+}
