2c1ea571c95433b5cf1c78e449da5f92f7f5451d
18291622484 <18291622484@163.com>
2020-12-08 19:44:05 +0800

    26.删除排序数组中的重复项 原地删除排序数组的重复项，返回移除后的数组长度。要求空间复杂度O(1)。

---
+++ 26.DeletesDuplicatesInTheSortedArray/26.DeletesDuplicatesInTheSortedArray.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int removeDuplicates(int* nums, int numsSize) {
+    int idx = 1;
+    int flag = nums[0];
+    int count = 1;
+    for (int i = 1; i < numsSize; i++) {
+        if (flag != nums[i]) {
+            nums[idx++] = nums[i];
+            flag = nums[i];
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    int num[] = { 1,2,2,3,6,8,8,9 };
+    int ret = removeDuplicates(num, sizeof(num) / sizeof(num[0]));
+    for (int i = 0; i < ret; i++) {
+        printf("%d\n", num[i]);
+    }
+	return 0;
+}
