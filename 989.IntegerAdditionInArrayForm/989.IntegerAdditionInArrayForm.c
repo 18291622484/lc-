464203a355c5d715e00f7a7ee568c42dd60fd6ed
18291622484 <18291622484@163.com>
2020-12-08 22:27:57 +0800

    989.数组形式的整数加法 对于非负整数X，有数组A是按照X从左到右每一位的顺序组成的数组，给定数组A，再给一个整数K，返回X+K的数组形式。

---
+++ 989.IntegerAdditionInArrayForm/989.IntegerAdditionInArrayForm.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int* addToArrayForm(int* A, int ASize, int K, int* returnSize) {
+    if (K == 0) {
+        *returnSize = ASize;
+        return A;
+    }
+    int count = (log10(K) + 1) > ASize ? (log10(K) + 2) : ASize + 1;
+    *returnSize = count;
+    int* ret = (int*)calloc(count, sizeof(int));
+    if (ret == NULL)
+        return NULL;
+    while (K > 0) {
+        if (--ASize >= 0)
+            K = A[ASize] + K;
+        ret[--count] = K % 10;
+        K /= 10;
+    }
+    if (ret[0] == 0) {
+        ret[--count] = K;
+        (*returnSize)--;
+        return ++ret;
+    }
+    else
+        return ret;
+}
+
+int main() {
+    int num[] = { 8,6,8 };
+    int k = 689;
+    int size = 0;
+    int* ret = addToArrayForm(num, sizeof(num) / sizeof(num[0]), k, &size);
+    for (int i = 0; i < size; i++) {
+        printf("%d", ret[i]);
+    }
+    printf("\n");
+	return 0;
+}
