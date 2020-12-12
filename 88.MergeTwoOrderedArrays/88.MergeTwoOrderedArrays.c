3260a6bb0e95275c71d4d1c19914e0f128074e27
18291622484 <18291622484@163.com>
2020-12-12 23:40:25 +0800

    稍作修改

--- 88.MergeTwoOrderedArrays/88.MergeTwoOrderedArrays.c
+++ 88.MergeTwoOrderedArrays/88.MergeTwoOrderedArrays.c
@@ -4,7 +4,7 @@
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
     int count = m + n - 1;
     m--, n--;
-    while (m >= 0 || n >= 0) {
+    while (m >= 0 && n >= 0) {
         if (nums1[m] < nums2[n]) {
             nums1[count] = nums2[n];
             count--;
@@ -24,9 +24,9 @@ void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
 }
 
 int main() {
-    int num1[] = { 1,2,3,6,0,0,0,0,0 };
+    int num1[] = { 6,8,9,10,11,15,0,0,0,0,0 };
     int num2[] = { 3,4,6,7,9 };
-    merge(num1, sizeof(num1) / sizeof(num1[0]), 4, num2, sizeof(num2) / sizeof(num2[0]), 5);
+    merge(num1, sizeof(num1) / sizeof(num1[0]), 6, num2, sizeof(num2) / sizeof(num2[0]), 5);
     for (int i = 0; i < sizeof(num1) / sizeof(num1[0]); i++) {
         printf("%d\n", num1[i]);
     }
