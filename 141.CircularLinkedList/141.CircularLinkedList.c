12a16833c65f9c591a1f5df6f2054921a9855f82
18291622484 <18291622484@163.com>
2020-12-14 11:23:27 +0800

    141. 环形链表 给定一个链表，判断链表中是否有环。考虑各种情况。

---
+++ 141.CircularLinkedList/141.CircularLinkedList.c
@@ -0,0 +1,18 @@
+int main() {
+    /*bool hasCycle(struct ListNode* head) {
+        if (head == NULL) {
+            return false;
+        }
+        struct ListNode* fast = head;
+        struct ListNode* slow = head;
+        while (fast != NULL && fast->next != NULL) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (slow >= fast && fast != NULL) {
+                return true;
+            }
+        }
+        return false;
+    }*/
+	return 0;
+}
