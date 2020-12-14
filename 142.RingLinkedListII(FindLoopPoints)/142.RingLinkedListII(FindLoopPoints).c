fb86c1b850285d77c29750ca5df0e63c56d9d7be
18291622484 <18291622484@163.com>
2020-12-14 16:09:10 +0800

    142. 环形链表 II（找循环点） 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。

---
+++ 142.RingLinkedListII(FindLoopPoints)/142.RingLinkedListII(FindLoopPoints).c
@@ -0,0 +1,25 @@
+int main() {
+    /*struct ListNode* detectCycle(struct ListNode* head) {
+        if (head == NULL) {
+            return false;
+        }
+        struct ListNode* fast = head;
+        struct ListNode* slow = head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        while (fast && fast->next) {
+            if (fast == head) {
+                return fast;
+            }
+            fast = fast->next;
+            head = head->next;
+        }
+        return NULL;
+    }*/
+	return 0;
+}
