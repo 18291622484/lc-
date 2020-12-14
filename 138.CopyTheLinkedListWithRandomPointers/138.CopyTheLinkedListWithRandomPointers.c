367ad870f78ba76b456849701dfc4e02883a1f61
18291622484 <18291622484@163.com>
2020-12-14 18:35:19 +0800

    138. 复制带随机指针的链表 给定一个链表，每个节点包含一个额外增加的随机指针，该指针可以指向链表中的任何节点或空节点。要求返回这个链表的深拷贝。

---
+++ 138.CopyTheLinkedListWithRandomPointers/138.CopyTheLinkedListWithRandomPointers.c
@@ -0,0 +1,48 @@
+int main() {
+    //struct Node* copyRandomList(struct Node* head) {
+    //    if (head == NULL) {
+    //        return NULL;
+    //    }
+    //    struct Node* tail = head;
+    //    while (tail) {
+    //        struct Node* temp = tail->next;
+    //        struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    //        node->val = tail->val;
+    //        tail->next = node;
+    //        node->next = temp;
+    //        tail = temp;
+    //    }
+    //    struct Node* node = head;
+    //    struct Node* cur = NULL;
+    //    while (node) {
+    //        cur = node->next;
+    //        if (node->random) {
+    //            cur->random = node->random->next;
+    //        }
+    //        else {
+    //            cur->random = NULL;
+    //        }
+    //        node = cur->next;
+    //    }
+    //    struct Node* p1 = head;
+    //    struct Node* p2 = head->next;
+    //    struct Node* p3 = NULL;
+    //    struct Node* ret = p2;
+    //    while (p2) {
+    //        if (p2->next) {
+    //            p3 = p2->next;
+    //            p1->next = p3;
+    //            p2->next = p3->next;
+    //            p1 = p3;
+    //            p2 = p1->next;
+    //        }
+    //        else {
+    //            p1->next = NULL;
+    //            p2->next = NULL;
+    //            p2 = p2->next;
+    //        }
+    //    }
+    //    return ret;
+    //}
+	return 0;
+}
