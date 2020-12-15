2d5fb05c40a750f4f3d821d266d655bc1391be8f
18291622484 <18291622484@163.com>
2020-12-15 15:05:23 +0800

    147. 对链表进行插入排序 对链表进行插入排序。

---
+++ 147.PerformInsertionSortOnLinkedLists/147.PerformInsertionSortOnLinkedLists.c
@@ -0,0 +1,35 @@
+int main() {
+    //struct ListNode* insertionSortList(struct ListNode* head) {
+    //    if (head == NULL || head->next == NULL) {
+    //        return head;
+    //    }
+    //    struct ListNode list;
+    //    list.next = head;
+    //    struct ListNode* tail = head;
+    //    struct ListNode* cur = tail->next;
+    //    while (cur) {
+    //        if (cur->val >= tail->val) {
+    //            tail = tail->next;
+    //            cur = cur->next;
+    //        }
+    //        else {
+    //            struct ListNode* node = cur;
+    //            cur = cur->next;
+    //            tail->next = cur;
+    //            struct ListNode* swap = list.next;
+    //            struct ListNode* last = &list;
+    //            while (swap != cur) {
+    //                if (swap->val >= node->val) {
+    //                    last->next = node;
+    //                    node->next = swap;
+    //                    break;
+    //                }
+    //                last = swap;
+    //                swap = swap->next;
+    //            }
+    //        }
+    //    }
+    //    return list.next;
+    //}
+	return 0;
+}
