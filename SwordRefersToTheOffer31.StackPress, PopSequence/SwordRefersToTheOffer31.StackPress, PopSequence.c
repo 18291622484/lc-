5eaaef908f65f0216c6f914a89a575a88a46cf6d
18291622484 <18291622484@163.com>
2021-01-01 21:36:46 +0800

    剑指 Offer 31. 栈的压入、弹出序列 输入两个整数序列，第一个序列表示栈的压入顺序，请判断第二个序列是否为该栈的弹出顺序。假设压入栈的所有数字均不相等。例如，序列 {1,2,3,4,5} 是某栈的压栈序列，序列 {4,5,3,2,1} 是该压栈序列对应的一个弹出序列，但 {4,3,5,1,2} 就不可能是该压栈序列的弹出序列。

---
+++ SwordRefersToTheOffer31.StackPress, PopSequence/SwordRefersToTheOffer31.StackPress, PopSequence.c
@@ -0,0 +1,25 @@
+int main() {
+    //bool validateStackSequences(int* pushed, int pushedSize, int* popped, int poppedSize) {
+    //    int* stackarr = (int*)malloc(pushedSize * sizeof(int));
+    //    int flag = pushedSize;
+    //    int flag1 = pushedSize;
+    //    for (int i = 0; flag1;) {
+    //        if (flag && (i == 0 || stackarr[i - 1] != *popped)) {
+    //            stackarr[i] = *pushed;
+    //            pushed++;
+    //            flag--;
+    //            i++;
+    //        }
+    //        if (stackarr[i - 1] == *popped) {
+    //            i--;
+    //            popped++;
+    //            flag1--;
+    //        }
+    //        else if (!flag) {
+    //            return false;
+    //        }
+    //    }
+    //    return true;
+    //}
+	return 0;
+}
