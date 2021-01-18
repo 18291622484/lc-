7741f1d4a6295d81935055d4061916d08704250c
18291622484 <18291622484@163.com>
2021-01-18 11:38:08 +0800

    965. 单值二叉树 如果二叉树每个节点都具有相同的值，那么该二叉树就是单值二叉树。 只有给定的树是单值二叉树时，才返回 true；否则返回 false。

---
+++ 965.Single-valuedBinaryTrees/965.Single-valuedBinaryTrees.c
@@ -0,0 +1,20 @@
+int main() {
+    /*bool _isSame(struct TreeNode* root, int n) {
+        if (root) {
+            return root->val == n
+                && _isSame(root->left, n)
+                && _isSame(root->right, n);
+        }
+        return true;
+    }
+
+    bool isUnivalTree(struct TreeNode* root) {
+        if (root == NULL) {
+            return true;
+        }
+        int n = root->val;
+        return _isSame(root, n);
+    }*/
+
+    return 0;
+}
