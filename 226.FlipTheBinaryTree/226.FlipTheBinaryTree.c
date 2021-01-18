9ac3de9373db40249771141e4a54b6ba473e9c38
18291622484 <18291622484@163.com>
2021-01-18 17:55:51 +0800

    226. 翻转二叉树 翻转一棵二叉树，使翻转前的二叉树和翻转后的二叉树呈镜相对称。

---
+++ 226.FlipTheBinaryTree/226.FlipTheBinaryTree.c
@@ -0,0 +1,14 @@
+int main() {
+    /*struct TreeNode* invertTree(struct TreeNode* root) {
+        if (root == NULL) {
+            return NULL;
+        }
+        struct TreeNode* temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }*/
+	return 0;
+}
