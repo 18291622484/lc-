ab3d9ac5434ce736cfabaa6eccd4e91246510feb
18291622484 <18291622484@163.com>
2021-01-18 15:18:54 +0800

    104. 二叉树的最大深度 给定一个二叉树，找出其最大深度。二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。

---
+++ 104.TheMaximumDepthOfTheBinaryTree/104.TheMaximumDepthOfTheBinaryTree.c
@@ -0,0 +1,12 @@
+int main() {
+    /*int maxDepth(struct TreeNode* root) {
+        if (root == NULL) {
+            return 0;
+        }
+        int _left = maxDepth(root->left);
+        int _right = maxDepth(root->right);
+        int ret = _left > _right ? _left + 1 : _right + 1;
+        return ret;
+    }*/
+	return 0;
+}
