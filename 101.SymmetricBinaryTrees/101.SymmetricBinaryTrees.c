aae8f1ac1dfb008b15702c95b21e8430263355f1
18291622484 <18291622484@163.com>
2021-01-18 20:01:17 +0800

    101. 对称二叉树 给定一个二叉树，检查它是否是镜像对称的。

---
+++ 101.SymmetricBinaryTrees/101.SymmetricBinaryTrees.c
@@ -0,0 +1,21 @@
+int main() {
+    /*bool _isSymmetric(struct TreeNode* _left, struct TreeNode* _right) {
+        if (_left == NULL && _right == NULL) {
+            return true;
+        }
+        if (_left == NULL || _right == NULL) {
+            return false;
+        }
+        return _left->val == _right->val
+            && _isSymmetric(_left->left, _right->right)
+            && _isSymmetric(_left->right, _right->left);
+    }
+
+    bool isSymmetric(struct TreeNode* root) {
+        if (root == NULL) {
+            return true;
+        }
+        return _isSymmetric(root->left, root->right);
+    }*/
+	return 0;
+}
