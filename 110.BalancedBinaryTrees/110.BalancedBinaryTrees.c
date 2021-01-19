93eeda4ed7a48acf511573e8782d99843f23aed3
18291622484 <18291622484@163.com>
2021-01-19 10:58:13 +0800

    110. 平衡二叉树 给定一个二叉树，判断它是否是高度平衡的二叉树。本题中，一棵高度平衡二叉树定义为：一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。

---
+++ 110.BalancedBinaryTrees/110.BalancedBinaryTrees.c
@@ -0,0 +1,22 @@
+int main() {
+    /*int deep(struct TreeNode* root) {
+        if (root == NULL) {
+            return 0;
+        }
+        int _left = deep(root->left);
+        int _right = deep(root->right);
+        return _left > _right ? _left + 1 : _right + 1;
+    }
+
+    bool isBalanced(struct TreeNode* root) {
+        if (root == NULL) {
+            return true;
+        }
+        int __left = deep(root->left);
+        int __right = deep(root->right);
+        return abs(__left - __right) < 2
+            && isBalanced(root->left)
+            && isBalanced(root->right);
+    }*/
+	return 0;
+}
