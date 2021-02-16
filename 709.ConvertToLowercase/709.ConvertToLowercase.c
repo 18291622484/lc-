da947910956218d592dce110d0c2701694c56bef
18291622484 <18291622484@163.com>
2021-02-16 17:01:46 +0800

    709. 转换成小写字母 实现函数ToLowerCase()，该函数接收一个字符串参数str，并将该字符串中的大写字母转换成小写字母，之后返回新的字符串。

---
+++ 709.ConvertToLowercase/709.ConvertToLowercase.c
@@ -0,0 +1,18 @@
+int main() {
+    /*char* toLowerCase(char* str) {
+        char* map1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        char* map2 = "abcdefghijklmnopqrstuvwxyz";
+        char* s = str;
+        while (*s) {
+            for (int i = 0; i < 26; i++) {
+                if (map1[i] == *s) {
+                    *s = map2[i];
+                    break;
+                }
+            }
+            s++;
+        }
+        return str;
+    }*/
+	return 0;
+}
