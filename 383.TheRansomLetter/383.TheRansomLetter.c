b36061ed83c85c702c6842f49de9c5592384da84
18291622484 <18291622484@163.com>
2021-02-19 19:09:04 +0800

    383. 赎金信 给定一个赎金信 (ransom) 字符串和一个杂志(magazine)字符串，判断第一个字符串 ransom 能不能由第二个字符串 magazines 里面的字符构成。如果可以构成，返回 true ；否则返回 false。(题目说明：为了不暴露赎金信字迹，要从杂志上搜索各个需要的字母，组成单词来表达意思。杂志字符串中的每个字符只能在赎金信字符串中使用一次)。

---
+++ 383.TheRansomLetter/383.TheRansomLetter.c
@@ -0,0 +1,23 @@
+int main() {
+    /*bool canConstruct(char* ransomNote, char* magazine) {
+        int ranarr[26] = { 0 };
+        int magarr[26] = { 0 };
+        while (*ransomNote) {
+            int i = (int)(*ransomNote);
+            ranarr[i - 97]++;
+            ransomNote++;
+        }
+        while (*magazine) {
+            int i = (int)(*magazine);
+            magarr[i - 97]++;
+            magazine++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (ranarr[i] && ranarr[i] > magarr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }*/
+	return 0;
+}
