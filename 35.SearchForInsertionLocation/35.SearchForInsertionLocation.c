7138574927b1771e5bda1657e4297b054f451ba9
18291622484 <18291622484@163.com>
2021-02-19 14:26:09 +0800

    35. 搜索插入位置 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。你可以假设数组中无重复元素。

---
+++ 35.SearchForInsertionLocation/35.SearchForInsertionLocation.c
@@ -0,0 +1,13 @@
+int main() {
+    /*int searchInsert(int* nums, int numsSize, int target) {
+        int i = 0;
+        while (i < numsSize) {
+            if (nums[i] >= target) {
+                return i;
+            }
+            i++;
+        }
+        return numsSize;
+    }*/
+	return 0;
+}
