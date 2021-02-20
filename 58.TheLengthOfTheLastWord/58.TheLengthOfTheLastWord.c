ca178f0b10fd1ba98940a4655207f8a81123e43f
18291622484 <18291622484@163.com>
2021-02-20 15:07:18 +0800

    58. 最后一个单词的长度 给你一个字符串s，由若干单词组成，单词之间用空格隔开。返回字符串中最后一个单词的长度。如果不存在最后一个单词，请返回0。单词是指仅由字母组成、不包含任何空格字符的最大子字符串。

---
+++ 58.TheLengthOfTheLastWord/58.TheLengthOfTheLastWord.c
@@ -0,0 +1,22 @@
+int main() {
+    /*int lengthOfLastWord(char* s) {
+        if (*s) {
+            char* m = s;
+            char* n = s;
+            while (*m) {
+                if (*m == ' ' && *(m + 1) && *(m + 1) != ' ') {
+                    n = m + 1;
+                }
+                m++;
+            }
+            int count = 0;
+            while (*n && *n != ' ') {
+                count++;
+                n++;
+            }
+            return count;
+        }
+        return 0;
+    }*/
+	return 0;
+}
