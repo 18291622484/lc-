32921217fd1019e6ffbfc05ec22c42f4b64ae8a6
18291622484 <18291622484@163.com>
2021-02-22 16:19:16 +0800

    977. 有序数组的平方 给你一个按 非递减顺序 排序的整数数组 nums，返回每个数字的平方组成的新数组，要求也按非递减顺序排序。

---
+++ 977.SquareOfAnOrderedArray/977.SquareOfAnOrderedArray.c
@@ -0,0 +1,21 @@
+int main() {
+    /*int* sortedSquares(int* nums, int numsSize, int* returnSize) {
+        int* ret = (int*)malloc(sizeof(int) * numsSize);
+        int m = 0;
+        int n = numsSize - 1;
+        int count = n;
+        while (m <= n) {
+            if (nums[m] + nums[n] >= 0) {
+                ret[count--] = nums[n] * nums[n];
+                n--;
+            }
+            else {
+                ret[count--] = nums[m] * nums[m];
+                m++;
+            }
+        }
+        *returnSize = numsSize;
+        return ret;
+    }*/
+	return 0;
+}
