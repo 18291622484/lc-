3be62c7d1846de1d9b3c35c37432c5fb9ec4de12
18291622484 <18291622484@163.com>
2021-02-22 16:43:44 +0800

    917. 仅仅反转字母 给定一个字符串 S，返回 “反转后的” 字符串，其中不是字母的字符都保留在原地，而所有字母的位置发生反转。

---
+++ 917.InvertLettersOnly/917.InvertLettersOnly.c
@@ -0,0 +1,33 @@
+int main() {
+    //bool fun(char* begin) {
+    //    if ((*begin > 64 && *begin < 91) || (*begin > 96 && *begin < 123)) {
+    //        return true;
+    //    }
+    //    return false;
+    //}
+    //char* reverseOnlyLetters(char* S) {
+    //    char* begin = S;
+    //    char* end = S;
+    //    while (*end) {
+    //        end++;
+    //    }
+    //    end--;
+    //    while (begin < end) {
+    //        if (!fun(begin)) {
+    //            begin++;
+    //        }
+    //        else if (!fun(end)) {
+    //            end--;
+    //        }
+    //        else {
+    //            char tmp = *begin;
+    //            *begin = *end;
+    //            *end = tmp;
+    //            begin++;
+    //            end--;
+    //        }
+    //    }
+    //    return S;
+    //}
+	return 0;
+}
