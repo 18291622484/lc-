8347d38cc4a958ac6543434ae03164125d49d1ba
18291622484 <18291622484@163.com>
2021-02-22 12:41:34 +0800

    925. 长按键入 你的朋友正在使用键盘输入他的名字 name。偶尔，在键入字符 c 时，按键可能会被长按，而字符可能被输入 1 次或多次。你将会检查键盘输入的字符 typed。如果它对应的可能是你的朋友的名字（其中一些字符可能被长按），那么就返回 True。

---
+++ 925.LongButtonEntry/925.LongButtonEntry.c
@@ -0,0 +1,19 @@
+int main() {
+    /*bool isLongPressedName(char* name, char* typed) {
+        char* n = name;
+        char* t = typed;
+        while (*n || *t) {
+            if (*t == *n || (n > name && (*t == *(n - 1) || *(n - 1) == *t))) {
+                if (*t == *n) {
+                    n++;
+                }
+                t++;
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }*/
+	return 0;
+}
