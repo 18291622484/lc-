2e184bc29c22a9f6493d27e20abe9f622bb8f86d
18291622484 <18291622484@163.com>
2021-02-23 11:32:19 +0800

    905. 按奇偶排序数组 给定一个非负整数数组 A，返回一个数组，在该数组中， A 的所有偶数元素之后跟着所有奇数元素。你可以返回满足此条件的任何数组作为答案。

---
+++ 905.SortArraysByEvenAndOdd/905.SortArraysByEvenAndOdd.c
@@ -0,0 +1,20 @@
+int main() {
+    //int* sortArrayByParity(int* A, int ASize, int* returnSize) {
+    //    *returnSize = ASize;
+    //    int begin = 0;
+    //    int end = ASize - 1;
+    //    while (begin < end) {
+    //        while (begin < end && A[begin] % 2 == 0) {
+    //            begin++;
+    //        }
+    //        while (begin < end && A[end] % 2 != 0) {
+    //            end--;
+    //        }
+    //        int tmp = A[begin];
+    //        A[begin] = A[end];
+    //        A[end] = tmp;
+    //    }
+    //    return A;
+    //}
+	return 0;
+}
