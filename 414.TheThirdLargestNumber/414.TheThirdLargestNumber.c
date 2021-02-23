219e5c52d30df7682cd83bf6388e8d9cc86d48ae
18291622484 <18291622484@163.com>
2021-02-23 21:25:30 +0800

    414. 第三大的数 给你一个非空数组，返回此数组中 第三大的数 。如果不存在，则返回数组中最大的数。

---
+++ 414.TheThirdLargestNumber/414.TheThirdLargestNumber.c
@@ -0,0 +1,27 @@
+int main() {
+    //#define MIN 比int类型最小值还要小的值(不知道该写什么好)
+    //int thirdMax(int* nums, int numsSize) {
+    //    int max = nums[0], mid = MIN, min = MIN;
+    //    for (int i = 0; i < numsSize; i++) {
+    //        if (nums[i] != max && nums[i] != mid && nums[i] != min) {
+    //            if (nums[i] > max) {
+    //                min = mid;
+    //                mid = max;
+    //                max = nums[i];
+    //            }
+    //            else if (nums[i] > mid) {
+    //                min = mid;
+    //                mid = nums[i];
+    //            }
+    //            else if (nums[i] > min) {
+    //                min = nums[i];
+    //            }
+    //        }
+    //    }
+    //    if (min != MIN) {
+    //        return min;
+    //    }
+    //    return max;
+    //}
+	return 0;
+}
