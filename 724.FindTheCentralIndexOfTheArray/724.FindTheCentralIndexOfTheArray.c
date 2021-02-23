8742ad2a4688d88ecade16ed563f795d2ff0ab34
18291622484 <18291622484@163.com>
2021-02-23 14:31:11 +0800

    724. 寻找数组的中心索引 给你一个整数数组 nums，请编写一个能够返回数组 “中心索引” 的方法。数组 中心索引 是数组的一个索引，其左侧所有元素相加的和等于右侧所有元素相加的和。如果数组不存在中心索引，返回 -1 。如果数组有多个中心索引，应该返回最靠近左边的那一个。注意：中心索引可能出现在数组的两端。

---
+++ 724.FindTheCentralIndexOfTheArray/724.FindTheCentralIndexOfTheArray.c
@@ -0,0 +1,17 @@
+int main() {
+    //int pivotIndex(int* nums, int numsSize) {
+    //    int sum = 0;
+    //    for (int i = 0; i < numsSize; i++) {
+    //        sum += nums[i];
+    //    }
+    //    int left = 0;
+    //    for (int i = 0; i < numsSize; i++) {
+    //        if (2 * left + nums[i] == sum) {
+    //            return i;
+    //        }
+    //        left += nums[i];
+    //    }
+    //    return -1;
+    //}
+	return 0;
+}
