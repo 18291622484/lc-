71adf3272be27289fd456e9ad1a5c4d277778405
18291622484 <18291622484@163.com>
2021-02-24 12:45:27 +0800

    1. 两数之和 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。你可以按任意顺序返回答案。

---
+++ 1.TheSumOfTheTwoNumbers/1.TheSumOfTheTwoNumbers.c
@@ -0,0 +1,17 @@
+int main() {
+    /*int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+        int* ret = (int*)malloc(sizeof(int) * 2);
+        *returnSize = 2;
+        for (int i = 0; i < numsSize - 1; i++) {
+            for (int j = i + 1; j < numsSize; j++) {
+                if (target - nums[i] == nums[j]) {
+                    ret[0] = i;
+                    ret[1] = j;
+                    return ret;
+                }
+            }
+        }
+        return ret;
+    }*/
+	return 0;
+}
