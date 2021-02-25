27c883764994f4465d3b47f379c152db42319718
18291622484 <18291622484@163.com>
2021-02-25 13:31:19 +0800

    8. 字符串转换整数 (atoi) 请你来实现一个 myAtoi(string s) 函数，使其能将字符串转换成一个 32 位有符号整数（类似 C/C++ 中的 atoi 函数）。

---
+++ 8.StringConversionInteger(ATOI)/8.StringConversionInteger(ATOI).c
@@ -0,0 +1,32 @@
+int main() {
+    //int myAtoi(char* s) {
+    //    long long num = 0;
+    //    char ch = '+';
+    //    int flag = 0;
+    //    while (*s && flag < 2 && num < 0x90000000) {
+    //        if (flag < 1 && *s == ' ') {
+    //            s++;
+    //        }
+    //        else if (flag < 1 && (*s == '-' || *s == '+')) {
+    //            ch = *s++;
+    //            flag++;
+    //        }
+    //        else if (*s >= '0' && *s <= '9') {
+    //            num *= 10;
+    //            num += *s++ - '0';
+    //            flag == 1 ? flag : flag++;
+    //        }
+    //        else {
+    //            break;
+    //        }
+    //    }
+    //    if (ch == '+' && num >= 0x7fffffff) {
+    //        return 0x7fffffff;
+    //    }
+    //    if (ch == '-' && num >= 0x80000000) {
+    //        return -0x80000000;
+    //    }
+    //    return ch == '-' ? -(int)num : (int)num;
+    //}
+	return 0;
+}
