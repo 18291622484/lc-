f1b70401ba2a492073d6d57098b213242f2780b1
18291622484 <18291622484@163.com>
2021-02-26 12:26:42 +0800

    125. 验证回文串 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。说明：本题中，我们将空字符串定义为有效的回文串。

---
+++ 125.VerifyPalindromeStrings/125.VerifyPalindromeStrings.c
@@ -0,0 +1,31 @@
+int main() {
+    //bool judge(char c) {
+    //    if ((c > 47 && c < 58) || (c > 64 && c < 91) || (c > 96 && c < 123)) {
+    //        return true;
+    //    }
+    //    return false;
+    //}
+    //bool isPalindrome(char* s) {
+    //    int n = strlen(s);
+    //    char* begin = s;
+    //    char* end = s + n - 1;
+    //    while (begin < end) {
+    //        if (!judge(*begin)) {
+    //            begin++;
+    //        }
+    //        if (!judge(*end)) {
+    //            end--;
+    //        }
+    //        if (judge(*begin) && judge(*end)) {
+    //            if ((*begin < 60 && *end > 60) || (*begin > 60 && *end < 60)
+    //                || *begin != *end && (*begin) + 32 != *end && (*begin) - 32 != *end) {
+    //                return false;
+    //            }
+    //            begin++;
+    //            end--;
+    //        }
+    //    }
+    //    return true;
+    //}
+	return 0;
+}
