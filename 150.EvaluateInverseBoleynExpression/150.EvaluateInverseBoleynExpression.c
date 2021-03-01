c096c73df8140239bdd7e76ea0b0c45e57875f87
18291622484 <18291622484@163.com>
2021-03-01 15:11:26 +0800

    150. 逆波兰表达式求值 根据逆波兰表示法，求表达式的值。有效的运算符包括 +, -, *, / 。每个运算对象可以是整数，也可以是另一个逆波兰表达式。说明：整数除法只保留整数部分；给定逆波兰表达式总是有效的。换句话说，表达式总会得出有效数值且不存在除数为 0 的情况。

---
+++ 150.EvaluateInverseBoleynExpression/150.EvaluateInverseBoleynExpression.c
@@ -0,0 +1,35 @@
+int main() {
+    //bool func(char* c) {
+    //    if ((*c == '+' || *c == '-' || *c == '*' || *c == '/') && *(c + 1) == '\0') {
+    //        return false;
+    //    }
+    //    return true;
+    //}
+    //int returnNum(int a, int b, char c) {
+    //    if (c == '+') {
+    //        return a + b;
+    //    }
+    //    else if (c == '-') {
+    //        return a - b;
+    //    }
+    //    else if (c == '*') {
+    //        return a * b;
+    //    }
+    //    return a / b;
+    //}
+    //int evalRPN(char** tokens, int tokensSize) {
+    //    int* stack = (int*)malloc(sizeof(int) * tokensSize);
+    //    for (int i = 0; i < tokensSize; i++) {
+    //        if (func(tokens[i])) {
+    //            *stack++ = atoi(tokens[i]);
+    //        }
+    //        else {
+    //            int b = *(--stack);
+    //            int a = *(--stack);
+    //            *stack++ = returnNum(a, b, *tokens[i]);
+    //        }
+    //    }
+    //    return *(--stack);
+    //}
+	return 0;
+}
